804f5ed4cf73f0970b86a656d3aaaf36844f865a
tmacks <tmacks1118@gmail.com>
2023-12-29 22:15:03 +0900

    マイグレーションファイルの記述

--- db/migrate/20231228002810_devise_create_users.rb
+++ db/migrate/20231228002810_devise_create_users.rb
@@ -5,9 +5,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: "",unique: true
+      t.string :nickname,           null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.date   :date_of_birth,      null: false
 
-      ## Recoverable
+      ## Recoverable :
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
