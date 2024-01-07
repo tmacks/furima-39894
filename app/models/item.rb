1086ac2ba0271d45ae62e27f0534c711da76740a
tmacks <tmacks1118@gmail.com>
2024-01-07 13:00:38 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
+  belongs_to :category
+  belongs_to :charge
+  belongs_to :condition
+  belongs_to :days_required
+  belongs_to :prefecture
   has_one_attached :image
+
+  validates :name, :string, presence: true
+  validates 
+  validates
+  validates
+  validates
+  validates
+  validates
+  validates
+  validates
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,6 @@ class User < ApplicationRecord
     validates :password,
               format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
   end
+
+  has_many :items
 end
