f2831954ac366c04646fa5ad4c1643699a423da4
tmacks <tmacks1118@gmail.com>
2024-01-07 21:44:36 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+  end
+ 
+  def new
+    @item = Item.new
   end
 
   def item_params
