7b5334b7af2c87cca1f7e12f04ef7c1b7b3cb0c5
tmacks <tmacks1118@gmail.com>
2024-01-09 20:31:09 +0900

    item_price.jsの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,6 +5,6 @@ priceInput.addEventListener("input", () => {
   const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor( inputValue * 0.1 )
   const profit = document.getElementById("profit");
-    profit.innerHTML = inputValue - addTaxDom.innerHTML
+    profit.innerHTML = Math.floor( inputValue - addTaxDom.innerHTML )
 })
 });
