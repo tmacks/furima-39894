eef608d3c29a6d74a3937a6975e51226b2d1cbbf
tmacks <tmacks1118@gmail.com>
2024-01-12 20:59:30 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -35,7 +35,11 @@ class ItemsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-
+  
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
   private
 
   def item_params
